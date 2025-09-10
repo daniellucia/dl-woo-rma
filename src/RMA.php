efc14eeafab8e29dd0f6f1664bebbdab288fd05b
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-10 07:02:20 +0200

    Comprobamos si existe la solcitud

--- src/RMA.php
+++ src/RMA.php
@@ -25,6 +25,11 @@ class RMA
      */
     public function create($order_id, $customer_id, $product_id, $reason, $comments)
     {
+
+        if ($this->exists($order_id, $product_id, $customer_id)) {
+            return 0; // Ya existe una solicitud de RMA para este pedido, producto y cliente
+        }
+
         $rma_data = [
             'post_title'   => __('RMA for Order #', 'dl-woo-rma') . ' ' . $order_id,
             'post_content' => $comments,
@@ -47,5 +52,43 @@ class RMA
         }
 
         return $this->id_rma;
+    }
+
+    /**
+     * Verifica si ya existe una solicitud de RMA para el mismo pedido, producto y cliente
+     * @param int $order_id
+     * @param int $product_id
+     * @param int $customer_id
+     * @return bool
+     * @author Daniel Lucia
+     */
+    public function exists(int $order_id, int $product_id, int $customer_id) {
+        $args = [
+            'post_type'   => 'rma',
+            'post_status' => 'any',
+            'meta_query' => [
+                [
+                    'key'     => '_rma_order_id',
+                    'value'   => $order_id,
+                    'compare' => '=',
+                ],
+                [
+                    'key'     => '_rma_product_id',
+                    'value'   => $product_id,
+                    'compare' => '=',
+                ],
+                [
+                    'key'     => '_rma_customer_id',
+                    'value'   => $customer_id,
+                    'compare' => '=',
+                ],
+            ],
+            'fields'      => 'ids',
+            'numberposts' => 1,
+        ];
+
+        $existing_rmas = get_posts($args);
+
+        return !empty($existing_rmas);
     }
 }
