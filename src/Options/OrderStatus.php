61e1cf20741d103e82eee64fd36065798d8d060c
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-30 06:49:34 +0200

    Comprobamos que el pedido no tenga todos los productos devueltos

--- src/Options/OrderStatus.php
+++ src/Options/OrderStatus.php
@@ -49,6 +49,15 @@ class OrderStatus
 
         $status = $order->get_status();
         
-        return in_array($status, $valid_statuses);
+        $valid = in_array($status, $valid_statuses);
+
+        //Obtenemos el porcetanje de productos con RMA en el pedido
+        $rma = new \DL\RMA\RMA();
+        $percent_rma = $rma->calculatePercentProductRMA($order->get_id());
+        if ($percent_rma >= 100) {
+            $valid = false;
+        }
+
+        return $valid;
     }
 }
